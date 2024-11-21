1bcab5d68f3ae7d30ca747cdbd66e7e49c7b30b6
Delitel <delitel1@mail.ru>
2024-11-21 20:21:41 +0300

    chore: Переход в папку со скриптом запуска

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,7 @@
 # Запуск сервера
 start_server(){
     send_message "Запускаем сервер..."
+    cd $VALHEIM_SERVER_PATH
     screen -S $SCREEN_SESSION_NAME -d -m bash $VALHEIM_SERVER_PATH/start_server.sh
     send_message "Сервер скоро будет доступен по адерсу: $external_ip:2456"
 }
